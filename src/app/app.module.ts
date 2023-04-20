077d6fc0757862342e1f03811d087cadae01491c
nikit <nikita.melamed@gmail.com>
2023-04-20 23:10:04 +0400

    модул

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
 
+const routes: Routes = [
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [ RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
