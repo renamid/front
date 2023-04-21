a9314d2d21b54628d57c4f9f6f7c6ebf127b973e
SQroller <cool.artyukov@gmail.com>
2023-04-21 08:35:27 +0300

    normal version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {RouterModule, Routes} from "@angular/router";
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
 ]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [ RouterModule.forRoot(routes),
     BrowserModule
